4125b6a289fbdd5a076a6e3ed8d6637249244ee2
Anshul Kuthe <145104113+anshulkuthe02@users.noreply.github.com>
2024-12-10 22:55:27 +0530

    Add files via upload

---
+++ PyDay1.py
@@ -0,0 +1,11 @@
+
+print("Welcome to AK's band")
+town = input("What is name of home town?")
+name = input("What's name of your pet?")
+print("The band name comes to be:",town,name)
+
+
+'''
+print(len(input("Enter d1: ")) + len(input("Enter d2:")))
+'''
+
